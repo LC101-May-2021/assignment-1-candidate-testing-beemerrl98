db9be4cd7d1afceabaa6d1e8e4a059492be33a87
beemerrl <>
2021-05-24 22:24:33 +0000

    v1, part 1 completed for class 3

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,13 +1,13 @@
-const input = require('readline-sync');
+const input = require('readline-sync'); 
 
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
 let candidateName;
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question = "Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride";
+let candidateAnswer = " ";
 let questions;
 let correctAnswers;
 let candidateAnswers;
@@ -15,22 +15,29 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+  candidateName = input.question('Dear Candidate, please enter your name: ');
+  console.log(`Greetings ${candidateName}. Welcome to the Entry Exam.`);
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
-
+  candidateAnswer = input.question(question);
 }
 
 function gradeQuiz(candidateAnswers) {
-
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 
+let grade;
 
-  let grade;
-  
+if(candidateAnswer === "Sally Ride") {
+  grade = '1/1';
+  console.log(`Correct! Your score is ${grade}`);
+  console.log(grade);
+} else {
+  grade = '0/1';
+  console.log(`Incorrect. Your score is ${grade}.`);
+}
+
 
   return grade;
 }
